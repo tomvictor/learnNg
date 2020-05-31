51cf34264dbe6a989e942cfb206ab47a96e433ef
Tom Victor <vjtomvictor@icloud.com>
2020-05-31 22:11:59 +0530

    *ngIf and *ngFor

--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -6,6 +6,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./product-list.component.css']
 })
 export class ProductListComponent implements OnInit {
+  products : any[] = [
+    {
+      "id": 1,
+      "name" : "Car",
+      "type" : "Automobile",
+      "price" : "$2000"
+    },
+    {
+      "id": 2,
+      "name" : "Antenna",
+      "type" : "Electronics",
+      "price" : "$100"
+    },
+    {
+      "id": 3,
+      "name" : "Weed",
+      "type" : "Medicine",
+      "price" : "$10"
+    }
+  ];
 
   constructor() { }
 
